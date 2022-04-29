4b1db0f412f99bdbba072fe13398402a8eb503fc
Bartosz <bar.brodowski@gmail.com>
2022-04-29 11:00:55 +0200

    Links, buttons styling

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import FacePhoto from '../img/facephoto.jpeg'
 
 const Bio = () => {
   return (
     <main className='bio'>
       <div className='image-container'>
-        <img alt="Page creator's face photo"></img>
+        <img src={FacePhoto} alt="Page creator's face"></img>
+        <div className='first-decorative-circle'></div>
+        <div className='second-decorative-circle'></div>
       </div>
       <div className="greeting">
           <div>Hi! I'm <span className="greeting-colored-text">Bartosz Brodowski</span></div>
