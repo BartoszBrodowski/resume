00cacc4848e81125007b3ece0f6a34026c0c5b3e
Bartosz Brodowski <bar.brodowski@gmail.com>
2022-05-01 17:57:44 +0200

    Added skills, TODO skill hover and projects

--- src/components/MainSection/Buttons.jsx
+++ src/components/MainSection/Buttons.jsx
@@ -5,9 +5,9 @@ const Buttons = () => {
   return (
     <div className="buttons-container">
         <ul className="buttons-list">
-            <a href="https://github.com/BartoszBrodowski" ><li className="buttons-list-item"><FaGithub className="buttons-list-item-icon" Github icon/></li></a>
-            <a href='https://gitlab.com/BartoszBrodowski' ><li className="buttons-list-item"><FaGitlab className="buttons-list-item-icon" /></li></a>
-            <a href="https://www.linkedin.com/in/bartosz-brodowski-425836235/"><li className="buttons-list-item"><FaLinkedin className="buttons-list-item-icon" /></li></a>
+            <a href="https://github.com/BartoszBrodowski" target={"_blank"}><li className="buttons-list-item"><FaGithub className="buttons-list-item-icon" Github icon/></li></a>
+            <a href='https://gitlab.com/BartoszBrodowski' target={"_blank"}><li className="buttons-list-item"><FaGitlab className="buttons-list-item-icon" /></li></a>
+            <a href="https://www.linkedin.com/in/bartosz-brodowski-425836235/" target={"_blank"}><li className="buttons-list-item"><FaLinkedin className="buttons-list-item-icon" /></li></a>
         </ul>
     </div>
   )
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,22 +2,34 @@ import React from 'react'
 import Python from "./img/python.svg"
 import JavaScript from "./img/javascript.svg"
 import { FaPython, FaJs, FaReact, FaSass } from "react-icons/fa"
+import { SiGnubash } from "react-icons/si"
+import { SiMicrosoftsqlserver } from "react-icons/si"
+import { DiGit, DiNpm } from "react-icons/di"
+import { FcLinux } from "react-icons/fc"
 
 
 const Skills = ({anotherRef}) => {
   return (
       <div id="skills-section" ref={anotherRef}>
-        <div className="skills-selection skills-section-good">
+        <div className="skills-section-good">
             <h2>Good Knowledge</h2>
             <div className="skills-section-good-icons">
                 <FaPython color="#3776AB" className="good-knowledge-icon"/>
                 <FaJs color="#F7DF1E" className="good-knowledge-icon"/>
                 <FaSass color="#CC6699" className="good-knowledge-icon" />
                 <FaReact color="#61DAFB" className="good-knowledge-icon" />
+                <DiGit color="#F05032" className="good-knowledge-icon" />
             </div>
         </div>
-        <div className="skills-selection skills-section-basic">
+        <hr className="skills-dividing-line" />
+        <div className="skills-section-basic">
             <h2>Basic Knowledge</h2>
+            <div className="skills-section-basic-icons">
+              <SiGnubash color="#4EAA25" className="basic-knowledge-icon" />
+              <FcLinux className="basic-knowledge-icon" />
+              <DiNpm color="#CB3837" className="basic-knowledge-icon" />
+              <SiMicrosoftsqlserver className="basic-knowledge-icon" />
+            </div>
         </div>
       </div>
   )
